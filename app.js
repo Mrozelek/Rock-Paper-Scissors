fec400d3aa1ce566ffbaf94ed95718a58bd7d67f
mrozelek <mrozelek@gmail.com>
2020-09-23 22:05:03 +0200

    Add local storage

--- app.js
+++ app.js
@@ -1,59 +1,43 @@
-(function () {
-    const introBtn = document.querySelector('.intro .btn');
-    const rockBtn = document.querySelector('.btn.rock');
-    const paperBtn = document.querySelector('.btn.paper');
-    const scissorsBtn = document.querySelector('.btn.scissors');
-
-    let duringRound = false;
-
-    function startGame() {
-        const introSection = document.querySelector('.intro');
-        const matchSection = document.querySelector('.match');
-
-        introSection.classList.add('fadeOut');
-        matchSection.classList.remove('fadeOut');
+class Game {
+    static loadPoints() {
+        Storage.loadPoints();
     }
 
-    function startRound(evt) {
-        if (duringRound) return;
-        duringRound = true;
-
-        const playerHand = document.querySelector('.player-hand');
-        const computerHand = document.querySelector('.computer-hand');
-
-        const playerMove = evt.target.classList[1];
-        const computerMove = randomComputerMove();
-
-        animateHand(playerHand, playerMove);
-        animateHand(computerHand, computerMove);
-
-        checkResult(playerMove, computerMove);
+    static startGame() {
+        Game.duringRound = false;
+        UI.introToMatchTransition();
     }
 
-    function animateHand(hand, move) {
-        setTimeout(() => {
-            hand.src = `./images/${move}.png`;
-        }, 300);
-        const handName = hand.classList[0].slice(0, -5);
-        hand.style.animation = `shake-hand-${handName} 1s ease`;
-        hand.addEventListener('animationend', removeAnimation);
+    static executeRound(evt) {
+        if (Game.duringRound === true) return;
+        Game.startRound();
+        const result = Game.executeMovesAndGetResult(evt);
+        Game.endRound(result);
     }
 
-    function removeAnimation() {
-        this.style.animation = '';
-        duringRound = false;
+    static startRound() {
+        Game.duringRound = true;
+        UI.animateShakeHands();
     }
 
-    function randomComputerMove() {
+    static executeMovesAndGetResult(evt) {
+        const playerMove = Game.getPlayerMove(evt);
+        UI.changeHandImage('player', playerMove);
+        const computerMove = Game.randomComputerMove();
+        UI.changeHandImage('computer', computerMove);
+        return Game.getRoundResult(playerMove, computerMove);
+    }
+
+    static getPlayerMove(evt) {
+        return evt.target.id;
+    }
+
+    static randomComputerMove() {
         const moves = ['rock', 'paper', 'scissors'];
-        return moves[getRandomInt(3)];
+        return moves[Utilities.getRandomInt(3)];
     }
 
-    function getRandomInt(max) {
-        return Math.floor(Math.random() * Math.floor(max));
-    }
-
-    function checkResult(playerMove, computerMove) {
+    static getRoundResult(playerMove, computerMove) {
         const possibleResults = new Map([
             ['rock rock', 'tie'],
             ['rock paper', 'computer'],
@@ -66,36 +50,152 @@
             ['scissors scissors', 'tie']
         ]);
         const moves = `${playerMove} ${computerMove}`;
-
-        incrementPoints(possibleResults.get(moves));
+        return possibleResults.get(moves);
     }
 
-    function incrementPoints(winner) {
-        winnerTag = document.querySelector('.winner');
-        let winnerText = '';
-
-        if (winner !== 'tie') {
-            pointsTag = document.querySelector(`.${winner}-score p`);
-            pointsTag.textContent = ++pointsTag.textContent;
-
-            winnerText = `The ${upperCaseFirst(winner)} has won!`;
-        } else {
-            winnerText = 'It\'s a tie.';
+    static endRound(result) {
+        if (result !== 'tie') {
+            const points = Storage.getPoints(result) + 1;
+            UI.changePointsText(result, points);
+            Storage.setPoints(result, points);
         }
+        UI.changeResultText(Utilities.makeResultText(result));
+        Game.checkHandsAnimationEnd();
+    }
 
+    static checkHandsAnimationEnd() {
+        UI.getHandTag('player').addEventListener('animationend', () => {
+            Game.duringRound = false;
+        });
+    }
+}
+
+class Storage {
+    static loadPoints() {
+        UI.changePointsText('player', Storage.getPoints('player'));
+        UI.changePointsText('computer', Storage.getPoints('computer'));
+    }
+
+    static getPoints(playerName) {
+        if (localStorage.getItem('punctation') === null) {
+            return 0;
+        }
+        const storedPunctations = JSON.parse(localStorage.getItem('punctation'));
+        for (const storedPunctation of storedPunctations) {
+            if (storedPunctation.playerName === playerName) {
+                return storedPunctation.points;
+            }
+        }
+    }
+
+    static setPoints(playerName, points) {
+        const player = {playerName: 'player', points: Storage.getPoints('player')};
+        const computer = {playerName: 'computer', points: Storage.getPoints('computer')};
+        const punctation = [player, computer];
+        punctation.map((player) => {
+            if (player.playerName === playerName) {
+                player.points = points;
+            }
+        });
+        localStorage.setItem('punctation', JSON.stringify(punctation));
+    }
+}
+
+class UI {
+    static introToMatchTransition() {
+        const introSection = document.querySelector('.intro');
+        const matchSection = document.querySelector('.match');
+
+        introSection.classList.add('fadeOut');
+        matchSection.classList.remove('fadeOut');
+    }
+
+    static changePointsText(playerName, points) {
+        UI.getPointsTag(playerName).textContent = points;
+    }
+
+    static animateShakeHands() {
+        for (const hand of UI.getHandsTag()) {
+            const handName = Utilities.getPlayer(hand);
+            hand.style.animation = `shake-hand-${handName} 1s ease`;
+            hand.addEventListener('animationend', UI.removeAnimation);
+        }
+    }
+
+    static removeAnimation() {
+        this.style.animation = '';
+    }
+
+    static changeHandImage(playerName, move) {
         setTimeout(() => {
-            winnerTag.textContent = winnerText;
+            UI.getHandTag(playerName).src = `./images/${move}.png`;
         }, 300);
     }
 
-    function upperCaseFirst(word) {
+    static changeResultText(result) {
+        setTimeout(() => {
+            UI.getResultTag().textContent = result;
+        }, 300);
+    }
+
+    static getPointsTag(playerName) {
+        return document.querySelector(`.${playerName}-score p`);
+    }
+
+    static getResultTag() {
+        return document.querySelector('.result');
+    }
+
+    static getHandsTag() {
+        return [UI.getHandTag('player'), UI.getHandTag('computer')];
+    }
+
+    static getHandTag(playerName) {
+        return document.querySelector(`.${playerName}-hand`);
+    }
+
+    static getIntroButton() {
+        return document.querySelector('.intro .btn');
+    }
+
+    static getRockButton() {
+        return document.querySelector('.btn.rock');
+    }
+
+    static getPaperButton() {
+        return document.querySelector('.btn.paper');
+    }
+
+    static getScissorsButton() {
+        return document.querySelector('.btn.scissors');
+    }
+}
+
+class Utilities {
+    static getPlayer(hand) {
+        return hand.classList[0].slice(0, -5);
+    }
+
+    static upperCaseFirst(word) {
         const firstLetter = word.charAt(0).toUpperCase();
         const rest = word.slice(1);
         return firstLetter + rest;
     }
 
-    introBtn.addEventListener('click', startGame);
-    rockBtn.addEventListener('click', startRound);
-    paperBtn.addEventListener('click', startRound);
-    scissorsBtn.addEventListener('click', startRound);
-})()
+    static getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    static makeResultText(result) {
+        if (result !== 'tie') {
+            return `The ${Utilities.upperCaseFirst(result)} has won!`;
+        }
+        return 'It\'s a tie.';
+    }
+}
+
+UI.getIntroButton().addEventListener('click', Game.startGame);
+UI.getRockButton().addEventListener('click', Game.executeRound);
+UI.getPaperButton().addEventListener('click', Game.executeRound);
+UI.getScissorsButton().addEventListener('click', Game.executeRound);
+document.addEventListener('DOMContentLoaded', Game.loadPoints);
